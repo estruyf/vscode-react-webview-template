fd0f1abb535a5b197c9d9111eccf34dda04190a3
Elio Struyf <elio@struyfconsulting.be>
2022-10-06 11:42:49 +0200

    Initial commit of the starter

---
+++ src/extension.ts
@@ -0,0 +1,77 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { join } from 'path';
+import * as vscode from 'vscode';
+import { ExtensionContext, ExtensionMode, Uri, Webview } from 'vscode';
+import { MessageHandlerData } from '@estruyf/vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('vscode-react-webview-starter.openWebview', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'react-webview',
+			'React Webview',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+				retainContextWhenHidden: true
+			}
+		);
+
+		panel.webview.onDidReceiveMessage(message => {
+			const { command, requestId, payload } = message;
+
+			console.log('Received message', command, requestId, payload);
+		
+			if (command === "GET_DATA") {
+				// Do something with the payload
+		
+				// Send a response back to the webview
+				panel.webview.postMessage({
+					command,
+					requestId, // The requestId is used to identify the response
+					payload: `Hello from the extension!`
+				} as MessageHandlerData<string>);
+			} else if (command === "POST_DATA") {
+				vscode.window.showInformationMessage(`Received data from the webview: ${payload.msg}`);
+			}
+		}, undefined, context.subscriptions);
+
+		panel.webview.html = getWebviewContent(context, panel.webview);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
+
+
+const getWebviewContent = (context: ExtensionContext, webview: Webview) => {
+	const jsFile = "webview.js";
+	const localServerUrl = "http://localhost:9000";
+
+	let scriptUrl = null;
+	let cssUrl = null;
+
+	const isProduction = context.extensionMode === ExtensionMode.Production;
+	if (isProduction) {
+		scriptUrl = webview.asWebviewUri(Uri.file(join(context.extensionPath, 'dist', jsFile))).toString();
+	} else {
+		scriptUrl = `${localServerUrl}/${jsFile}`; 
+	}
+
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		${isProduction ? `<link href="${cssUrl}" rel="stylesheet">` : ''}
+	</head>
+	<body>
+		<div id="root"></div>
+
+		<script src="${scriptUrl}" />
+	</body>
+	</html>`;
+}
---
+++ src/webview/App.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { messageHandler } from '@estruyf/vscode/dist/client';
+import "./styles.css";
+
+export interface IAppProps {}
+
+export const App: React.FunctionComponent<IAppProps> = ({ }: React.PropsWithChildren<IAppProps>) => {
+  const [message, setMessage] = React.useState<string>("");
+
+  const sendMessage = () => {
+    messageHandler.send('POST_DATA', { msg: 'Hello from the webview' });
+  };
+
+  const requestData = () => {
+    messageHandler.request<string>('GET_DATA').then((msg) => {
+      setMessage(msg);
+    });
+  };
+
+  return (
+    <div className='app'>
+      <h1>Hello from the React Webview Starter</h1>
+
+      <div className='app__actions'>
+        <button onClick={sendMessage}>
+          Send message to extension
+        </button>
+
+        <button onClick={requestData}>
+          Get data from extension
+        </button>
+      </div>
+
+      {message && <p><strong>Message from the extension</strong>: {message}</p>}
+    </div>
+  );
+};
